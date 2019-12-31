90089e8fba4546e729e4b36c1357b7e7e1d23a47
Pablo Llinás <pebs74@gmail.com>
2019-12-31 08:30:52 +0100

    comments

--- get_temp.sh
+++ get_temp.sh
@@ -1,1 +1,2 @@
+# temp/humid sensor connected on raspi pin 4
 python ./Adafruit_Python_DHT/examples/AdafruitDHT.py 11 4
--- turn_off.sh
+++ turn_off.sh
@@ -1,3 +1,4 @@
+# Heater switch connected to raspi pin 2
 gpio -g mode 2 out
 gpio -g write 2 1
 echo "OFF" > ./curr_state
